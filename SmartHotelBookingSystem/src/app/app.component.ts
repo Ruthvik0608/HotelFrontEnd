846a5a11d20468f4ba824208018349834e19c030
Ruthvik0608 <2393150@cognizant.com>
2025-04-26 17:50:38 +0530

    Added NewLogin Page Component

--- SmartHotelBookingSystem/src/app/app.component.ts
+++ SmartHotelBookingSystem/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "./Components/nav-bar/nav-bar.component";
+import { LoginPageComponent } from "./Components/login-page/login-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
